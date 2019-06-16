6193da1be1968e26e24a3ba728073702ba3ade82
Stephen <7speter@gmail.com>
2019-06-15 21:34:38 -0400

    Updates

---
+++ precache-manifest.30824c94dbcadb208e6e29113a546087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b924666786bb8e6a0ae3408ab2aae3",
+    "url": "/reactPomodoroApp/index.html"
+  },
+  {
+    "revision": "e93b834677b4f74734bb",
+    "url": "/reactPomodoroApp/static/css/main.6882037e.chunk.css"
+  },
+  {
+    "revision": "f52837dee97d9868a609",
+    "url": "/reactPomodoroApp/static/js/2.58c37161.chunk.js"
+  },
+  {
+    "revision": "e93b834677b4f74734bb",
+    "url": "/reactPomodoroApp/static/js/main.d3349406.chunk.js"
+  },
+  {
+    "revision": "2b6da91737a506fed1eb",
+    "url": "/reactPomodoroApp/static/js/runtime~main.233e0f60.js"
+  }
+]);
